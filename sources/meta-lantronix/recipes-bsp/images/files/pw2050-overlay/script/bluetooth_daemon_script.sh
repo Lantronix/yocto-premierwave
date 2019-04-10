2ff4cc67b5a0ec509e7394223dd1a0ae64342840
Suresh Reddy <skreddy@lantronix.com>
2019-04-10 15:42:25 +0530

    Update to the last commit.

--- sources/meta-lantronix/recipes-bsp/images/files/pw2050-overlay/script/bluetooth_daemon_script.sh
+++ sources/meta-lantronix/recipes-bsp/images/files/pw2050-overlay/script/bluetooth_daemon_script.sh
@@ -1,10 +1,8 @@
 mknod -m 666 /dev/rfcomm0 c 216 0
 mkdir /var/run/dbus/
 dbus-daemon --config-file /etc/dbus-1/system.conf --fork
-bluetoothd&
+/usr/libexec/bluetooth/bluetoothd --noplugin=sap --compat&
 chmod 777 /var/run/sdp
 hciconfig hci0 sspmode 0
 hciconfig hci0 piscan
-sdptool add --channel=3 SP
-rfcomm watch /dev/rfcomm0 3&
 
