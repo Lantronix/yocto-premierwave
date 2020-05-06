5e341bf74c6e00d22e83cd84db8d1d5ad34f106b
Suresh Reddy <skreddy@lantronix.com>
2020-05-06 09:45:44 +0530

    Updating bat_c2 version to 9.3.1.0R4

--- sources/meta-lantronix/recipes-bsp/images/files/overlay/usr/lib/cfginterface.py
+++ sources/meta-lantronix/recipes-bsp/images/files/overlay/usr/lib/cfginterface.py
@@ -3,6 +3,11 @@ from ctypes import *
 
 _library = ctypes.CDLL('libsecurity.so')
 
+def ltrx_status_get(groups, xcr, messages):
+    global _library
+    result = _library.ltrx_status_get(ctypes.c_char_p(groups),ctypes.c_char_p(xcr),ctypes.c_char_p(messages))
+    return result
+
 def ltrx_cfg_get(groups, xcr, messages):
     global _library
     result = _library.ltrx_cfg_get(ctypes.c_char_p(groups),ctypes.c_char_p(xcr),ctypes.c_char_p(messages))
